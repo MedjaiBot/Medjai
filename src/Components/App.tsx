00111f57ae8523d1277abbb9f962ab3925afd1e8
Yannick Fricke <yannickfricke@googlemail.com>
2018-04-30 21:53:40 +0200

    feat(Added App component): Added the App component and added public/index.html

---
+++ src/Components/App.tsx
@@ -0,0 +1,9 @@
+import * as React from 'react';
+
+export class App extends React.Component<{}, {}> {
+    render () {
+        return <div>
+            <p>Hello World!</p>
+        </div>;
+    }
+}
